d3af573c17f98b2f15b2cfb4cd69598e46845647
nithish-mucherla <mucherlanithu@gmail.com>
2021-02-04 17:27:09 +0530

    add .css and .js files

---
+++ home.js
@@ -0,0 +1,70 @@
+const validate = () => {
+  const from = document.getElementById("countryCodesFrom").value;
+  const to = document.getElementById("countryCodesTo").value;
+  const amount = document.getElementById("fromValue").value;
+  if (!from) {
+    document.getElementById("countryCodeFromError").classList.remove("hidden");
+    return;
+  }
+
+  if (!to) {
+    document.getElementById("countryCodeToError").classList.remove("hidden");
+    return;
+  }
+
+  if (!amount) {
+    document.getElementById("fromValueError").classList.remove("hidden");
+    return;
+  }
+
+  if (amount <= 0) {
+    let errorDiv = document.getElementById("fromValueError");
+    errorDiv.innerHTML = "Please Enter a correct value";
+    errorDiv.classList.remove("hidden");
+    return;
+  }
+  convert();
+};
+const loadCountryCodes = async () => {
+  let response = await fetch(
+    "https://currency-converter13.p.rapidapi.com/list",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3d15874e0amshf1bdb48a55c5499p100e08jsn9ee781838fa3",
+        "x-rapidapi-host": "currency-converter13.p.rapidapi.com",
+      },
+    }
+  );
+  return response.json();
+};
+
+const convert = async () => {
+  const from = document.getElementById("countryCodesFrom").value;
+  const to = document.getElementById("countryCodesTo").value;
+  const amount = document.getElementById("fromValue").value;
+  let response = await fetch(
+    `https://currency-converter13.p.rapidapi.com/convert?from=${from}&to=${to}&amount=${amount}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3d15874e0amshf1bdb48a55c5499p100e08jsn9ee781838fa3",
+        "x-rapidapi-host": "currency-converter13.p.rapidapi.com",
+      },
+    }
+  );
+  response.json().then((data) => {
+    document.getElementById("toValue").value = data.amount;
+  });
+};
+
+const loadData = () => {
+  loadCountryCodes().then((data) => {
+    optionList = `<option value="" disabled selected>select country code</option>`;
+    data.forEach(
+      (code) => (optionList += `<option value=${code}>${code}</option>`)
+    );
+    document.getElementById("countryCodesFrom").innerHTML = optionList;
+    document.getElementById("countryCodesTo").innerHTML = optionList;
+  });
+};
